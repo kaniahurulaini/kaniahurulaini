62f6fd3b4b75bc8fae0c73fe12680a46b8b0a192
kaniahurulaini <64120793+kaniahurulaini@users.noreply.github.com>
2020-09-09 11:40:24 +0700

    Add files via upload

---
+++ Javauts_kania.java
@@ -0,0 +1,24 @@
+package Javauts_kania;
+
+public class Javauts_kania {
+    public static void main (String[] args) {
+        String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+     char Ch1 = str.charAt(10);
+     char Ch2 = str.charAt(0);
+     char Ch3 = str.charAt(13);
+     char Ch4 = str.charAt(8);
+     char Ch5 = str.charAt(0);
+     
+     
+     
+     System.out.println (Ch1);
+     System.out.println (Ch2);
+     System.out.println (Ch3);
+     System.out.println (Ch4);
+     System.out.println (Ch5);
+     System.out.println(""+ str.charAt(10) + str.charAt(0) + str.charAt(13) +  str.charAt(8) + str.charAt(0) + str.charAt(27) + str.charAt(7) +  str.charAt(20) + str.charAt(17)+ str.charAt(20) +  str.charAt(11) + str.charAt(27) + str.charAt(0));
+
+     
+    }
+}
+
